d0aa986db8687a5eed5d93180d810bbfa204c391
Nick King <nick.king@cambiahealth.com>
2022-02-10 01:48:39 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.13.0"
   bucket_prefix = "${prefix}"
-
-  versioning = {
-    enabled = true
-  }
-
+  # insert the 5 required variables here
 }
